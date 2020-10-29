7405033f7d0878941dbb5ab2e3bcf2331ad79c76
Albert <abonmassip@gmail.com>
2020-10-29 10:36:22 -0700

    last last name

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
-const name = 'Albert Bonmassip'
+const name = 'Albert Bonmassip Bove'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
